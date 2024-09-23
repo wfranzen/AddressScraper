74c26f1a7e6355c31063ad8810d587925d5ebac8
wfranzen <89801028+wfranzen@users.noreply.github.com>
2024-09-23 15:24:27 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="address_scraper",
+    version="0.1.0",
+    author="Will Franzen",
+    author_email="willfranzen@gmail.com",
+    description="A Python library for normalizing and extracting address components",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wfranzen/AddressScraper",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+    include_package_data=True,
+)
