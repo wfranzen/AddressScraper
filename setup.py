09a6a6a94b0627b317719a3d1a1e7dd6af2bf607
wfranzen <89801028+wfranzen@users.noreply.github.com>
2024-09-23 15:48:55 -0400

    File Restructuring

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/wfranzen/AddressScraper",
-    packages=find_packages(include=["*"]),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
